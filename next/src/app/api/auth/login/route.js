91d826a2a8dd750420e212e75ca3870f88aef6f0
Afaaf Hariri <afaafhariri@icloud.com>
2025-06-28 10:09:21 +0800

    login with mongoose

--- next/src/app/api/auth/login/route.js
+++ next/src/app/api/auth/login/route.js
@@ -1,40 +1,26 @@
+import connectDB from "@/lib/mongoose";
+import User from "@/models/User";
 import { NextResponse } from "next/server";
-import clientPromise from "@/lib/mongodb";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export async function POST(req) {
-  try {
-    const { email, password } = await req.json();
+  await connectDB();
 
-    if (!email || !password) {
-      return NextResponse.json(
-        { error: "Email and password required" },
-        { status: 400 }
-      );
-    }
+  const { email, password } = await req.json();
 
-    const client = await clientPromise;
-    const db = client.db(); // auto-uses DB name from URI
-    const user = await db.collection("users").findOne({ email });
-
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return NextResponse.json(
-        { error: "Invalid credentials" },
-        { status: 401 }
-      );
-    }
-
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "7d",
-    });
-
-    return NextResponse.json({ token }, { status: 200 });
-  } catch (error) {
-    console.error("Login error:", error);
-    return NextResponse.json(
-      { error: "Internal server error" },
-      { status: 500 }
-    );
+  if (!email || !password) {
+    return NextResponse.json({ error: "Missing credentials" }, { status: 400 });
   }
+
+  const user = await User.findOne({ email });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
+  }
+
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  return NextResponse.json({ token }, { status: 200 });
 }
