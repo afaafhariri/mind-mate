9f2df13432eebb46db838112ca3ae487e154f19f
Afaaf Hariri <afaafhariri@icloud.com>
2025-07-03 18:56:51 +0530

    connectdb import path

--- next/src/app/api/auth/signup/route.js
+++ next/src/app/api/auth/signup/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
-import connectDB from "@/lib/mongoose";
+import connectDB from "lib/mongoose";
 import User from "@models/users";
 
 export async function POST(req) {
