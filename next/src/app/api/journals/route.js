7de9a2110930c788d480f9d172b484add12253ea
Afaaf Hariri <afaafhariri@icloud.com>
2025-07-06 21:25:11 +0530

    journals

--- next/src/app/api/journals/route.js
+++ next/src/app/api/journals/route.js
@@ -1,40 +1,38 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
 import jwt from "jsonwebtoken";
-
-const prisma = new PrismaClient();
+import connectDB from "@/lib/mongoose";
+import Journal from "@/models/journal";
+import User from "@/models/users";
 
 export async function POST(req) {
+  await connectDB();
+
   const authHeader = req.headers.get("authorization");
   const token = authHeader?.split(" ")[1];
 
-  const { topic, body, location } = await req.json();
-
-  if (!token)
+  if (!token) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const { topic, body, location } = await req.json();
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const journal = await prisma.journal.create({
-      data: {
-        topic,
-        body,
-        location,
-        userId: decoded.userId,
-      },
+
+    const journal = await Journal.create({
+      topic,
+      body,
+      location,
+      userId: decoded.userId,
     });
 
-    await prisma.user.update({
-      where: { id: decoded.userId },
-      data: {
-        totalJournals: {
-          increment: 1,
-        },
-      },
+    await User.findByIdAndUpdate(decoded.userId, {
+      $inc: { totalJournals: 1 },
     });
+
     return NextResponse.json(journal, { status: 201 });
   } catch (error) {
-    console.error(error);
+    console.error("Error creating journal:", error);
     return NextResponse.json(
       { error: "Error creating journal" },
       { status: 500 }
@@ -43,21 +41,25 @@ export async function POST(req) {
 }
 
 export async function GET(req) {
+  await connectDB();
+
   const authHeader = req.headers.get("authorization");
   const token = authHeader?.split(" ")[1];
 
-  if (!token)
+  if (!token) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const journals = await prisma.journal.findMany({
-      where: { userId: decoded.userId },
-      orderBy: { createdAt: "desc" },
+
+    const journals = await Journal.find({ userId: decoded.userId }).sort({
+      createdAt: -1,
     });
-    return NextResponse.json(journals);
+
+    return NextResponse.json(journals, { status: 200 });
   } catch (error) {
-    console.error(error);
+    console.error("Error fetching journals:", error);
     return NextResponse.json(
       { error: "Error fetching journals" },
       { status: 500 }
