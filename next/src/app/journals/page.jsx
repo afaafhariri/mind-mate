61e31a8cf3e8f1ec1670cc6dd69d8aaa99f2f02f
Afaaf Hariri <afaafhariri@icloud.com>
2025-07-07 17:08:37 +0530

    delete id

--- next/src/app/journals/page.jsx
+++ next/src/app/journals/page.jsx
@@ -227,7 +227,7 @@ function JournalsPage() {
                   </button>
                   <button
                     onClick={async () => {
-                      await handleDelete(journalToDelete.id);
+                      await handleDelete(journalToDelete._id);
                       setJournalToDelete(null);
                     }}
                     className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
