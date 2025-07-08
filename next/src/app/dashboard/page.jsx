79b330061e576807f0fcfb1adf934e72be3232d8
Afaaf Hariri <afaafhariri@icloud.com>
2025-07-08 20:24:11 +0530

    dashboard

--- next/src/app/dashboard/page.jsx
+++ next/src/app/dashboard/page.jsx
@@ -1,7 +1,5 @@
 "use client";
 import NavBar from "@components/nav-bar";
-import Header from "@components/header";
-import Footer from "@components/footer";
 import Chatbot from "@components/chat-bot";
 import UserRouteProtection from "@components/user-route";
 import Dashboard from "@components/dashboard";
@@ -11,18 +9,11 @@ function DashboardPage() {
     <UserRouteProtection>
       <div className="flex min-h-screen bg-gray-50">
         <NavBar currentPage="dashboard" />
-
         <div className="flex-1 ml-20 transition-all duration-300">
-          <Header />
-
-          <main className="flex-grow">
+          <main className="flex items-center justify-center min-h-screen">
             <Dashboard />
           </main>
-
-          <Footer />
         </div>
-
-        {/* Chatbot */}
         <Chatbot />
       </div>
     </UserRouteProtection>
