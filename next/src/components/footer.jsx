3ac98685625df9ba991408781d9830190b3a4be0
Afaaf Hariri <afaafhariri@icloud.com>
2025-07-12 19:21:01 +0530

    name

--- next/src/components/footer.jsx
+++ next/src/components/footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="bg-[var(--background)] text-[var(--foreground)] px-8 py-5 flex justify-end items-center ">
       <p className="text-sm mr-2">
-        made by <span className="font-bold">WE_IT_105</span>
+        made by <span className="font-bold">Afaaf Hariri</span>
       </p>
       <nav className=""></nav>
     </footer>
