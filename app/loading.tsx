90ecad358dd84eee5892581178034f9740400ac8
Afaaf Hariri <afaafhariri@icloud.com>
2025-09-30 00:13:15 +0530

    fix: splash screen icon matches prefered color scheme now

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,13 +5,26 @@ export default function Loading() {
     return (
         <div className="fixed inset-1 z-[9999] flex flex-col items-center justify-center bg-background/90 text-foreground">
             <h2 className="text-xl font-semibold mb-4 animate-pulse">Mind Mate.</h2>
-            <Image
+            {/* <Image
                 src="/web-app-manifest-512x512.png"
                 alt="Loading"
                 width={100}
                 height={100}
                 className="animate-pulse"
-            />
+            /> */}
+            <div className="w-[100px] h-[100px] animate-pulse">
+                <Image
+                    src="/meditation-round-svgrepo-com.svg"
+                    alt="Loading"
+                    width={100}
+                    height={100}
+                    className="filter"
+                    style={{
+                        filter: 'brightness(0) saturate(100%) invert(var(--foreground-invert, 0))'
+                    }}
+                    priority
+                />
+            </div>
         </div>
     )
 }
