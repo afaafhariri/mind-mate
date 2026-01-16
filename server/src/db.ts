e72af6a95c7a44ef1a13a40e454872d8a3d89bb8
Afaaf Hariri <afaafhariri@icloud.com>
2026-01-16 23:02:03 +0530

    fix: migrate to firebase cloud firestore

---
+++ server/src/db.ts
@@ -0,0 +1,14 @@
+import * as fireBaseAdmin from "firebase-admin";
+import { getFirestore } from "firebase-admin/firestore";
+import serviceAccount from "../mind-mate-firebase-admin-sdk.json";
+
+const serviceAccountObj = serviceAccount as fireBaseAdmin.ServiceAccount;
+
+if (!fireBaseAdmin.apps.length) {
+  fireBaseAdmin.initializeApp({
+    credential: fireBaseAdmin.credential.cert(serviceAccountObj),
+  });
+}
+
+export const db = getFirestore();
+
